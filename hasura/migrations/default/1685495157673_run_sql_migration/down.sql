e0f861a0c88c65770a27e4b8c07fee44d0ae38ad
Jesse Martin <themotleydev@gmail.com>
2023-05-30 21:10:32 -0400

    first metadata

---
+++ hasura/migrations/default/1685495157673_run_sql_migration/down.sql
@@ -0,0 +1,40 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- -- Create the schema for additional user info
+-- CREATE SCHEMA user_info;
+--
+-- -- Create the "user" table in the main schema
+-- CREATE TABLE "user" (
+--   user_id SERIAL PRIMARY KEY,
+--   user_name VARCHAR(100),
+--   date_of_birth DATE,
+--   gender VARCHAR(10),
+--   prescribing_doctor VARCHAR(100)
+-- );
+--
+-- -- Create the "user_info" table in the user_info schema
+-- CREATE TABLE user_info.user_info (
+--   user_id INT PRIMARY KEY,
+--   address VARCHAR(100),
+--   contact_number VARCHAR(20),
+--   email VARCHAR(100),
+--   prescription_support BOOLEAN
+-- );
+--
+-- -- Create the "billing_info" table in the user_info schema
+-- CREATE TABLE user_info.billing_info (
+--   user_id INT PRIMARY KEY,
+--   card_number VARCHAR(16),
+--   cardholder_name VARCHAR(100),
+--   expiration_date DATE,
+--   cvv VARCHAR(4)
+-- );
+--
+-- -- Create the "device_info" table in the user_info schema
+-- CREATE TABLE user_info.device_info (
+--   user_id INT,
+--   device_id SERIAL PRIMARY KEY,
+--   device_name VARCHAR(100),
+--   prescription_support BOOLEAN,
+--   FOREIGN KEY (user_id) REFERENCES "user" (user_id)
+-- );
